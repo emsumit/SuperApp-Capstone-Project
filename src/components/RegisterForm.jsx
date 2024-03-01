485946219508b992de3d23a91d7f02cb3d90e31c
emsumit <72928644+emsumit@users.noreply.github.com>
2024-03-01 11:01:32 +0530

    added error handling & route

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import "./RegisterForm.css";
 import registerPage from "../assets/register-page.png";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterForm() {
   const [formValues, setFormValues] = useState({
@@ -11,9 +12,57 @@ export default function RegisterForm() {
     mobile: "",
     checkbox: false,
   });
+  const [errors, setErrors] = useState({
+    name: null,
+    username: null,
+    email: null,
+    mobile: null,
+    checkbox: null,
+  });
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+  const handleSignUp = (e) => {
+    let isError = false;
+    // e.preventDefault();
+    if (formValues.name.trim().length === 0) {
+      setErrors((prev) => ({ ...prev, name: "Name is Required" }));
+      isError = true;
+    } else {
+      setErrors((prev) => ({ ...prev, name: null }));
+    }
+    if (formValues.username.trim().length === 0) {
+      setErrors((prev) => ({ ...prev, username: "Username is Required" }));
+      isError = true;
+    } else {
+      setErrors((prev) => ({ ...prev, username: null }));
+    }
+    if (formValues.email.trim().length === 0) {
+      setErrors((prev) => ({ ...prev, email: "Email is Required" }));
+      isError = true;
+    } else {
+      setErrors((prev) => ({ ...prev, email: null }));
+    }
+    if (formValues.mobile.trim().length === 0) {
+      setErrors((prev) => ({ ...prev, mobile: "Mobile number is Required" }));
+      isError = true;
+    } else {
+      setErrors((prev) => ({ ...prev, mobile: null }));
+    }
+    if (formValues.checkbox === false) {
+      setErrors((prev) => ({
+        ...prev,
+        checkbox: "You need to Agree T&Cs is required",
+      }));
+      isError = true;
+    } else {
+      setErrors((prev) => ({ ...prev, checkbox: null }));
+    }
+    if (!isError) {
+      navigate("/movies");
+    }
   };
   return (
     <>
@@ -35,6 +84,11 @@ export default function RegisterForm() {
               onChange={handleChange}
               name="name"
             />
+            {errors.name ? (
+              <p style={{ color: "red", margin: 0 }}>{errors.name}</p>
+            ) : (
+              <></>
+            )}
             <input
               type="text"
               placeholder="Username"
@@ -42,6 +96,11 @@ export default function RegisterForm() {
               onChange={handleChange}
               name="username"
             />
+            {errors.username ? (
+              <p style={{ color: "red", margin: 0 }}>{errors.username}</p>
+            ) : (
+              <></>
+            )}
             <input
               type="email"
               placeholder="Email"
@@ -49,6 +108,11 @@ export default function RegisterForm() {
               onChange={handleChange}
               name="email"
             />
+            {errors.email ? (
+              <p style={{ color: "red", margin: 0 }}>{errors.email}</p>
+            ) : (
+              <></>
+            )}
             <input
               type="text"
               placeholder="Mobile"
@@ -56,6 +120,11 @@ export default function RegisterForm() {
               onChange={handleChange}
               name="mobile"
             />
+            {errors.mobile ? (
+              <p style={{ color: "red", margin: 0 }}>{errors.mobile}</p>
+            ) : (
+              <></>
+            )}
             <section>
               <input
                 type="checkbox"
@@ -68,7 +137,12 @@ export default function RegisterForm() {
                 Share my registration data with Superapp
               </label>
             </section>
-            <button>SIGN UP</button>
+            {errors.checkbox ? (
+              <p style={{ color: "red", marginTop: 0 }}>{errors.checkbox}</p>
+            ) : (
+              <></>
+            )}
+            <button onClick={handleSignUp}>SIGN UP</button>
             <p>
               By clicking on Sign up. you agree to Superapp{" "}
               <mark>Terms and Conditions of Use</mark>
